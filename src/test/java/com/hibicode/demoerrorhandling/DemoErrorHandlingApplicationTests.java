314366d98addbc9ac1b07ba008521497e675d6d9
Normandes José Moreira Junior <tqi_nmoreira@uolinc.com>
2018-06-18 16:40:28 -0300

    First commit

---
+++ src/test/java/com/hibicode/demoerrorhandling/DemoErrorHandlingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hibicode.demoerrorhandling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoErrorHandlingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
